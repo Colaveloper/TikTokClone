98ea744b6fdbded9472a7227c2d6dd99d0339a86
Colaveloper <63930332+Colaveloper@users.noreply.github.com>
2020-09-03 23:50:30 +0200

    started tiktok clone

---
+++ tik-tok-clone/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCGUZ-Wy9c9YpGsdGQwEQj1BTzst49t13o",
+  authDomain: "tiktokclone-17d1a.firebaseapp.com",
+  databaseURL: "https://tiktokclone-17d1a.firebaseio.com",
+  projectId: "tiktokclone-17d1a",
+  storageBucket: "tiktokclone-17d1a.appspot.com",
+  messagingSenderId: "172871007317",
+  appId: "1:172871007317:web:bcbe82fc335ff783bf28dc",
+  measurementId: "G-5LZP47NRF1",
+};
